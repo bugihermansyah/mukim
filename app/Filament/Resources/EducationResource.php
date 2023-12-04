62f8001c1d4631e203a647e35542d9a79e799ed3
Bugi Hermansyah <bugihermansyah@gmail.com>
2023-12-04 09:33:40 +0700

    add resource education

---
+++ app/Filament/Resources/EducationResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EducationResource\Pages;
+use App\Filament\Resources\EducationResource\RelationManagers;
+use App\Models\Education;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EducationResource extends Resource
+{
+    protected static ?string $model = Education::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-briefcase';
+    
+    protected static ?string $navigationLabel = 'Pendidikan';
+
+    protected static ?string $pluralModelLabel = 'Pendidikan';
+
+    protected static ?string $navigationParentItem = 'Warga';
+    
+    protected static ?string $navigationGroup = 'Manajemen Warga';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEducation::route('/'),
+            'create' => Pages\CreateEducation::route('/create'),
+            'edit' => Pages\EditEducation::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
