6d23b6d18708080b0a09ed6a30ae3d03e208bfe5
Bugi Hermansyah <bugihermansyah@gmail.com>
2023-12-04 09:59:04 +0700

    fix view warga

--- app/Filament/Resources/WargaResource/Pages/ViewWarga.php
+++ app/Filament/Resources/WargaResource/Pages/ViewWarga.php
@@ -2,32 +2,13 @@
 
 namespace App\Filament\Resources\WargaResource\Pages;
 
-use App\Enums\ConfirmStatus;
 use App\Filament\Resources\WargaResource;
-use App\Models\Warga;
-use EightyNine\Approvals\Models\ApprovableModel;
-use EightyNine\Approvals\Traits\HasApprovalHeaderActions;
-use Filament\Actions;
-use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
-use EightyNine\Approvals\Forms\Actions\ApproveAction;
-use EightyNine\Approvals\Forms\Actions\DiscardAction;
-use EightyNine\Approvals\Forms\Actions\RejectAction;
-use EightyNine\Approvals\Forms\Actions\SubmitAction;
 
 class ViewWarga extends ViewRecord
 {
-    use  HasApprovalHeaderActions;
 
     protected static string $resource = WargaResource::class;
-
-    protected function getOnCompletionAction(): Action
-    {
-        return 
-            Action::make("Done")
-                ->color("success");
-                // ->hidden(fn(ApprovableModel $record)=> $record->shouldBeHidden());
-    }
 
     // protected function getActions(): array
     // {
